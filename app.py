2920f3b97f63629efc2ae029f1ab3ee50bc403e2
XinyuC2 <sparkleily@gmail.com>
2020-07-30 13:29:45 -0400

    workable search

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'Crave_for_it'
 # os.getenv("NAME_DB") 
 # URI of database for read/write provileges
-app.config['MONGO_URI'] = 'mongodb+srv://admin:d87vF5jh8wyifr0j@cluster0.amcew.mongodb.net/Crave_for_it?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = 'mongodb+srv://admin2:DqhHmSEzYZodCgXF@cluster0.amcew.mongodb.net/Crave_for_it?retryWrites=true&w=majority'
 mongo = PyMongo(app)
 # -- Routes section --
 # INDEX
@@ -19,22 +19,34 @@ mongo = PyMongo(app)
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
-
-@app.route('/finder')
+@app.route('/finder', methods=['GET', 'POST'])
 def finder():
-    collection=mongo.db.Restaurants
-    # # collection.insert_one({'name':'test', 'hours':'test name', 'allergen': 'none'})
-    restaurants=collection.find({})
-    # print(restaurants)
-    return render_template("finder.html", time=datetime.now(), restaurants = restaurants)
-    
-@app.route('/getinfo', methods=['GET', 'POST'])
-def info():
-    if request.method == 'GET':
-        collection = mongo.db.Restaurants
-        collection.find({ })
-        return render_template("finder.html", collection=list(collection), time=datetime.now)
+    if request.method =="GET":
+        collection=mongo.db.Restaurants
+        restaurants=collection.find({})
+        return render_template('finder.html', time=datetime.now(), restaurants= [ ])
     else:
-        peanuts = request.form["peanuts"]
-        shellfish = request.form["shellfish"]
-        lactose = request.form["lactose"]
+        print (request.form)
+        collection = mongo.db.Restaurants
+        restaurants=collection.find ({ })
+        if 'peanut-free' in dict(request.form):
+            restaurants = collection.find({'peanut-free':'true'})
+        # peanuts = request.form['peanut']
+        # shellfish = request.form['shellfish']
+        # soy = request.form['soy']
+        # lactose = request.form['lactose']
+        # vegan = request.form['vegan']
+        # gluten = request.form['gluten']
+        # kosher = request.form['kosher']
+        # peanuts == (collection.find({'peanut': 'true'})) 
+        #      list(collection.find({'name', 'hours', 'contact'}))
+        # elif shellfish == (collection.find({'shellfish': 'true' })):
+        #     list(collection.find({'name', 'hours', 'contact'}))
+        # elif soy == (collection.find ({'soy': 'true'})):
+        # elif lactose == (collection.find ({'lactose': 'true'})):
+        # elif vegan ==  (collection.find ({'vegan': 'true'})):
+        # elif gluten ==  (collection.find ({'gluten': 'true'})):
+        # elif kosher ==  (collection.find ({'kosher': 'true'})):
+            # else:
+            #     return (collection.find({ })
+    return render_template("finder.html", restaurants=restaurants, time=datetime.now)
